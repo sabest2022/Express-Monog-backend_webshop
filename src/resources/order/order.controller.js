69e7d4b28ef0630d6bdf6b5e537a034e0d10201d
heinanX <larrii@live.se>
2023-02-01 13:14:31 +0100

    getting weird error

--- src/resources/order/order.controller.js
+++ src/resources/order/order.controller.js
@@ -12,16 +12,37 @@ async function createOrder(req, res, next) {
     for (items of [order]){
         for (item of items.orderItems){
             let product = await ProductModel.findById(item.product);
-            console.log(product.inStock)
-            console.log(item.quantity)
             
             // if (product.inStock - (items.quantity < 0)) { return res.status(400).json("Sold out.") }
             product.inStock -= item.quantity;
             await product.save();
         }
     }
-
     res.status(201).json(order)
 }
 
-module.exports = { createOrder }
+async function getAllOrders(req, res) {
+    try {
+        if (req.session.user.isAdmin === true) {
+            const allOrders = await OrderModel.find({})
+            return res.status(200).json(allOrders);
+        }
+        const user = req.session.user._id
+        const orders = await OrderModel.find({ user: user})
+        res.status(200).json(orders)
+    } catch {
+        res.status(404).json("user " + user + " has no orders yet.")
+    }
+}
+
+
+
+
+
+async function getOrderId(req, res) {
+    const user = req.session.user._id
+    console.log(user);
+    // const orders = await OrderModel.find{}
+}
+
+module.exports = { createOrder, getAllOrders, getOrderId }
--- src/resources/order/order.router.js
+++ src/resources/order/order.router.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { createOrder } = require('./order.controller');
+const { createOrder, getAllOrders, getOrderId } = require('./order.controller');
 const { isAdmin, isLoggedIn, validate } = require("../middleware/middleware");
 const orderRouter = express.Router();
 
 // Order routes
+orderRouter.get('/', isLoggedIn, getAllOrders);
+orderRouter.get('/:id', isLoggedIn, getOrderId);
 orderRouter.post('/', isLoggedIn, createOrder);
 
 
