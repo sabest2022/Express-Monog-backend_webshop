ab967a333d58b08a9b0556228ce6cd04a4087505
Sara Pellnor <114143422+SaraPellnor@users.noreply.github.com>
2023-02-02 16:11:41 +0100

    laaaast

--- src/resources/order/order.router.js
+++ src/resources/order/order.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { orderJoiSchema } = require('./order.model');
-const { createOrder, getAllOrders, getOrderId } = require('./order.controller');
+const { createOrder, getAllOrders, getOrderId, isShipped } = require('./order.controller');
 const { isAdmin, isLoggedIn, validate } = require("../middleware/middleware");
 const orderRouter = express.Router();
 
@@ -8,6 +8,7 @@ const orderRouter = express.Router();
 orderRouter.get('/', isLoggedIn, getAllOrders);
 orderRouter.get('/:id', isLoggedIn, getOrderId);
 orderRouter.post('/', isLoggedIn, createOrder);
+orderRouter.put('/:id', isAdmin, isShipped);
 
 // validate(orderJoiSchema),
 module.exports = { orderRouter };
